4e572adb348489b18cecf20a66a4fc4d6ca53e48
Favian <damin8@naver.com>
2019-12-19 19:19:13 +0900

    update

--- WorkSpace/JSPProject/src/com/Damin/web/Nana.java
+++ WorkSpace/JSPProject/src/com/Damin/web/Nana.java
@@ -16,10 +16,17 @@ public class Nana extends HttpServlet{
 			HttpServletResponse rep) throws IOException, ServletException
 	{
 	  rep.setCharacterEncoding("UTF-8"); // 사용자가 보내는 코딩 방식을 결정!
-	  rep.setContentType("text/html; charset=UTF-8"); // 받았을 때 어떤 방식으로 해석할거냐 결정
-	  PrintWriter out = rep.getWriter();
-	  for(int i=1;i<=100;i++) {
-		  out.println((i)+" : 성경준 병신<br >");
-	  }
+    rep.setContentType("text/html; charset=UTF-8"); // 받았을 때 어떤 방식으로 해석할거냐 결정
+    PrintWriter out = rep.getWriter();
+    String count = req.getParameter("cnt");
+    int cnt = 3;
+    if(count != null && !count.equals(""))
+      cnt = Integer.parseInt(count); // 서로 약속을 해야 한다 (서버는 cnt를 보낼 것을 알고 있어야 한다.)
+              // 문자열이 오니까 parseInt를 이용해 Int로 변환
+              
+    for(int i=0;i<cnt;i++) {
+    	out.println((i+1) +" 안녕하세요 <br>");
+    }
+      //이렇게 활용 가능
 	}
 }
