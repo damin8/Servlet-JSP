40b966d3af02fe6839ecb276fa6282f0ccf6b4a1
Favian <damin8@naver.com>
2020-01-09 08:57:13 +0900

    update
    입력 데이터 배열로 받기
    
    session, applicaiton을 이용해 저장된 값 활용하기

--- WorkSpace/JSPProject/src/com/Damin/web/Calc.java
+++ WorkSpace/JSPProject/src/com/Damin/web/Calc.java
@@ -1,31 +1,46 @@
 package com.Damin.web;
 
 import java.io.IOException;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 
 @WebServlet("/calc")
 public class Calc extends HttpServlet {
 	
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ServletContext application = request.getServletContext();
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html; charset=UTF-8");
-		String x_ = request.getParameter("x");
-		String y_ = request.getParameter("y");
+		HttpSession session = request.getSession();
+		String v = request.getParameter("v");
+		String op = request.getParameter("operator");
 		
-		int x = 0;
-		int y = 0;
+		int v_ = 0;
+		if(!v.equals("")) v_ = Integer.parseInt(v);
 		
-		if(!x_.equals("")) x = Integer.parseInt("x_");
-		if(!y_.equals("")) y = Integer.parseInt("y_");
-		
-		int result = x + y;
-		
-		response.getWriter().printf("result is %d\n", result);
+		if(op.equals("=")) {
+			//int x = (Integer)application.getAttribute("value");
+			int x = (Integer)session.getAttribute("value");
+			int y = v_;
+			int result = 0;
+			String op_ = (String)session.getAttribute("op");
+			//String op_ = (String)application.getAttribute("op");
+			if(op.equals("+")) result = x + y;
+			else result = x - y;
+		}
+		else {
+			//application.setAttribute("value", v);
+			//application.setAttribute("op", op);
+			session.setAttribute("value", v);
+			session.setAttribute("op", op);
+		}
 	}
 
 }
